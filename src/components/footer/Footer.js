63cbafa7a30d4f6bff115e75489082a5089abcd2
Rishab Singh <rishab.w.singh1234@gmail.com>
2023-07-05 11:20:42 +0530

    gd

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,10 +4,12 @@ import './footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='footerHireContainer '>
-        <p>Let's connect and discuss how I can bring value to your organization and contribute to its growth and success.</p>
-        <div>
-          <a href='mailto:w3b.dev.rishu@gmail.com' className='footerHireMeBtn'>Hire Me</a>
+      <div className='footerResponsiveContainer'>
+        <div className='footerHireContainer '>
+          <p>Let's connect and discuss how I can bring value to your organization and contribute to its growth and success.</p>
+          <div>
+            <a href='mailto:w3b.dev.rishu@gmail.com' className='footerHireMeBtn'>Hire Me</a>
+          </div>
         </div>
       </div>
     </div>
